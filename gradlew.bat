0a6ef9f1b4725bd3586555f40531f560bc7a9efb
Leonorenexus <leonorenexus@gmail.com>
2026-07-03 19:11:00 +0700

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,6 @@
+@rem Gradle startup script for Windows
+@if "%DEBUG%"=="" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+set DIRNAME=%~dp0
+java -jar "%DIRNAME%\gradle\wrapper\gradle-wrapper.jar" %*
